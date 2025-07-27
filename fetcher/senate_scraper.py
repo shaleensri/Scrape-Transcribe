947091301214bec171a999eb8d6e15ea8d132ed8
Shaleen Srivastava <shaleen0601@gmail.com>
2025-07-27 02:39:40 -0400

    Senate scrapping for list of videos. will return video ids

---
+++ fetcher/senate_scraper.py
@@ -0,0 +1,70 @@
+# fetcher/senate_scraper.py
+
+from .base_scraper import BaseScraper
+import requests
+from datetime import datetime
+import re
+
+class SenateScraper(BaseScraper):
+    def __init__(self):
+        super().__init__("https://tf4pr3wftk.execute-api.us-west-2.amazonaws.com/default/api/all")
+        self.api_payload = {
+            "_id": "61b3adc8124d7d000891ca5c",
+            "page": 1,
+            "results": 2000
+        }
+
+    def fetch_data(self):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        response = requests.post(self.base_url, json=self.api_payload, headers=headers)
+        response.raise_for_status()
+        return response.json().get("allFiles", [])
+
+    def parse_recording_date(self, filename: str) -> str:
+        """
+        Extracts 'YY-MM-DD' from filename and returns 'YYYY-MM-DD'
+        """
+        match = re.search(r"(\d{2})-(\d{2})-(\d{2})", filename)
+        if match:
+            yy, mm, dd = match.groups()
+            try:
+                return datetime.strptime(f"{yy}-{mm}-{dd}", "%y-%m-%d").strftime("%Y-%m-%d")
+            except ValueError:
+                pass
+        return "Unknown"
+
+    def scrape(self) -> list:
+        data = self.fetch_data()
+        results = []
+
+        for item in data:
+            video_id = item.get("_id")
+            metadata = item.get("metadata", {})
+            filename = metadata.get("filename", "Untitled")
+            upload_date_raw = item.get("date", "")
+
+            # Extract dates
+            recording_date = self.parse_recording_date(filename)
+            try:
+                upload_date = datetime.fromisoformat(upload_date_raw.rstrip("Z")).strftime("%Y-%m-%d")
+            except Exception:
+                upload_date = upload_date_raw
+
+            results.append({
+                "video_id": video_id,
+                "title": filename,
+                "recording_date": recording_date,
+                "upload_date": upload_date
+            })
+
+        return results
+
+
+if __name__ == "__main__":
+    scraper = SenateScraper()
+    videos = scraper.scrape()
+    for video in videos[:5]:  # Show sample
+        print(video)
